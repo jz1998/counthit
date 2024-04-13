2fd1a9647666f66887048702619dee7aaabe3f8e
Johnson <35098802+jz1998@users.noreply.github.com>
2024-04-13 15:51:01 +0800

    Update and rename hit-counter@1.3.js to hit-counter@1.3.1.js

--- hit-counter@1.3.1.js
+++ hit-counter@1.3.1.js
@@ -162,11 +162,11 @@ class EmojiReaction extends s {
       }
     </style>
     <!-- container -->
-    <div>
+    <div class="${this?.theme === 'dark' || (this?.theme === 'system' && system_theme === 'dark') ? 'container-dark' : 'container'}">
       <!-- reactions got -->
       <div style="display: flex; gap: 0.375rem; flex-wrap: wrap; justify-content: center;">
         ${this.availableReactions.map(item => x`
-          <div @click=${this._react} data-name="${item.reaction_name}" class="${item.meReacted ? 'reaction-got-reacted' : 'reaction-got-not-reacted'}" style="display: flex; user-select: none; cursor: pointer; justify-content: center; align-items: center; padding: 0.25rem; font-size: 3.25rem; font-weight: 1000;">
+          <div @click=${this._react} data-name="${item.reaction_name}" class="${item.meReacted ? 'reaction-got-reacted' : 'reaction-got-not-reacted'}" style="display: flex; user-select: none; cursor: pointer; justify-content: center; align-items: center; padding: 0.25rem 1.75rem; font-size: 3.25rem; font-weight: 1000;">
             <span style="pointer-events: none;">${item.emoji}</span><span style="padding:0 0.375rem; pointer-events: none;">${String(item.count || 0).padStart(5, '0')}</span>
           </div>
         `)}
